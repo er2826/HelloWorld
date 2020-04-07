9c4ebd75d23410156e8e87a660e1f943e2a03251
er2826 <63190126+er2826@users.noreply.github.com>
2020-04-07 18:54:47 +1000

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 # This is my first python project!
 first = 'John'
 last = 'Smith'
-message = first +'[' + last + '] is a coder'
+message = first + '[' + last + '] is a coder'
 msg = f'{first} [{last}] is coder'
 print(message)
+
+
+def greeting():
+    print("Find you")
